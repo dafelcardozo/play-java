d123286134b64211cbc1fb1c37982b8313a0dff8
david.cardozo <david.cardozo@gonetusa.com>
2015-09-28 13:40:05 -0500

    Botón 'eliminar'.

--- public/javascripts/example.js
+++ public/javascripts/example.js
@@ -1,68 +1,47 @@
 $(document).ready(function() {
-/*
-    $.get("/persons", function(persons) {
-      $.each(persons, function(index, person) {
-           $("#persons tbody").append("<tr><td>"+person.fname+"</td><td>"+person.lname+"</td><td><button data-person='"+person.personId+"'>X</button></td></tr>");
-      });
-      setTimeout(function(){
-        $("table button").puibutton();
-        $("table").puidatatable();
-      }, 1000);
-
-    });
-*/
      $("table").puidatatable({
-            columns: [
-                {field:'fname', headerText: 'Nombre', sortable:true},
-                {field:'lname', headerText: 'Apellido', sortable:true},
-                {}
-            ],
-         datasource: function(callback) {
-             $.ajax({
-                 type: "GET",
-                 url: '/persons',
-                 dataType: "json",
-                 context: this,
-                 success: function(response) {
-                     callback.call(this, response);
-//                     $("table button").puibutton();
-                 }
-             });
-         },
+        columns: [
+            {field:'fname', headerText: 'Nombre', sortable:true},
+            {field:'lname', headerText: 'Apellido', sortable:true}
+        ],
+        selectionMode:'single',
+        datasource: function(callback) {
+         $.ajax({
+             type: "GET",
+             url: '/persons',
+             dataType: "json",
+             context: this,
+             success: function(response) {
+                 callback.call(this, response);
+             }
+         });
+        },
+        rowSelect: function(event, data) {
+          personId = data.personId;
+        },
      });
 
-    $("#persons").on("click", "button", function() {
-        var id = ""+$(this).data("person");
 
-        $.ajax({
-            url: '/eliminar?personId='+id,
-            type: 'DELETE',
-            success: function(result) {
-                location.reload();
-            }
-        });
-    });
     $("input").puiinputtext();
     $("#formDiv").puidialog({
-            showEffect : 'fade',
-            hideEffect : 'fade',
-            minimizable : true,
-            maximizable : true,
-            width       : 600,
-            modal : true,
-            location : 'top',
-            title:'Hola mundo',
-            dialog_title:"Hola mundo",
-             buttons : [ {
-                        text : 'Agregar',
-                        icon : 'ui-icon-check',
-                        click : function()
-                        {
-                            $("form").submit();
-                         //   $('#dlg').puidialog('hide');
-                        }
-                    }],
-     });
+        showEffect : 'fade',
+        hideEffect : 'fade',
+        minimizable : true,
+        maximizable : true,
+        width       : 600,
+        modal : true,
+        location : 'top',
+        title:'Hola mundo',
+        dialog_title:"Hola mundo",
+        buttons : [ {
+            text : 'Agregar',
+            icon : 'ui-icon-check',
+            click : function()
+            {
+                $("form").submit();
+            }
+        }]
+    });
 
     $("#mostrarDialog").click(function() {
         $("#formDiv").puidialog('show');
@@ -72,8 +51,18 @@ $(document).ready(function() {
 
     addMessage('warn', {summary: 'Una aplicaci&oacute;n de ejemplo - ', detail: 'Esta es mi aplicaci&oacute;n de ejemplo'});
 
+    $("#eliminar").click(function() {
+        $.ajax({
+            url: '/eliminar?personId='+personId,
+            type: 'DELETE',
+            success: function(result) {
+                location.reload();
+            }
+        });
+    });
 });
 
+var personId ;
 
 addMessage = function(severity, msg) {
     $('h1').puimessages('show', severity, msg);
